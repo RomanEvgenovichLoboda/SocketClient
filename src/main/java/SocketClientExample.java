68ebf019828674aada933aa46f5e13d0e434e70f
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-03-01 14:42:03 +0200

    allTasks

--- src/main/java/SocketClientExample.java
+++ src/main/java/SocketClientExample.java
@@ -11,11 +11,6 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-/**
- * This class implements java socket client
- * @author pankaj
- *
- */
 public class SocketClientExample {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
@@ -31,9 +26,9 @@ public class SocketClientExample {
             socket = new Socket(host.getHostName(), 9876);
             switch (menu){
                 case "1"->{
-                    System.out.print("Enter Login");
+                    System.out.print("Enter Login: ");
                     String log = consol.nextLine();
-                    System.out.print("Enter Password");
+                    System.out.print("Enter Password: ");
                     String pass = consol.nextLine();
                     MyData mD = new MyData(new User(log,pass),false);
                     var myDjo = new JSONObject(mD);
@@ -42,17 +37,16 @@ public class SocketClientExample {
                     oos.writeObject(str);
                     ois = new ObjectInputStream(socket.getInputStream());
                     String message = (String) ois.readObject();
-                    System.out.println("Message: " + message);
+                    System.out.println("\nMessage: " + message);
                     ois.close();
                     oos.close();
                 }
                 case "2"->{
-                    System.out.print("Enter Login");
+                    System.out.print("Enter Login: ");
                     String log = consol.nextLine();
-                    System.out.print("Enter Password");
+                    System.out.print("Enter Password: ");
                     String pass = consol.nextLine();
                     User us = new User(log,pass);
-                    //User us = new User("rrr","ttt");
                     MyData mD = new MyData(us,true);
                     var myDjo = new JSONObject(mD);
                     String str = myDjo.toString();
@@ -60,41 +54,17 @@ public class SocketClientExample {
                     oos.writeObject(str);
                     ois = new ObjectInputStream(socket.getInputStream());
                     String message = (String) ois.readObject();
-                    System.out.println("Message: " + message);
+                    System.out.println("\nMessage: " + message);
                     ois.close();
                     oos.close();
                 }
                 case "0"->{
                     oos = new ObjectOutputStream(socket.getOutputStream());
                     oos.writeObject("exit");
-                    //ois = new ObjectInputStream(socket.getInputStream());
-                    //String message = (String) ois.readObject();
-                    //System.out.println("Message: " + message);
-                    //ois.close();
                     oos.close();
                     return;
                 }
             }
-
         }
-
-
-//        for(int i=0; i<5;i++){
-//            //establish socket connection to server
-//            socket = new Socket(host.getHostName(), 9876);
-//            //write to socket using ObjectOutputStream
-//            oos = new ObjectOutputStream(socket.getOutputStream());
-//            System.out.println("Sending request to Socket Server");
-//            if(i==4)oos.writeObject("exit");
-//            else oos.writeObject(""+i);
-//            //read the server response message
-//            ois = new ObjectInputStream(socket.getInputStream());
-//            String message = (String) ois.readObject();
-//            System.out.println("Message: " + message);
-//            //close resources
-//            ois.close();
-//            oos.close();
-//            Thread.sleep(100);
-//        }
     }
 }
